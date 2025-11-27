8e844a71fbac06af333a72cfcca76b183deb99b2
Marcela Mosquera <march.jim.05@gmail.com>
2025-11-27 08:12:39 -0500

    Cambio de Divisas

--- fundamentos/lib/cambioDivisas.dart
+++ fundamentos/lib/cambioDivisas.dart
@@ -1,10 +1,29 @@
-void main() {}
+void main() {
+  print.chelinesAPesetas();
+  print.dracmasAFrancos();
+  print.pesetasADolaresYLiras();
+}
 
 void chelinesAPesetas() {
-  var chelinesPesetas = 100 / 956.871;
+  var chelinesPesetas = 100 / 956871;
   var chelines = 50;
   var pesetas = chelines * chelinesPesetas;
   print("El valor de $chelines chelines a pesetas es: $pesetas");
 }
 
-void dracmasAFrancos() {}
+void dracmasAFrancos() {
+  var dracmasPesetas = 100 / 88607;
+  var dracmas = 45;
+  var dracmasTotal = dracmasPesetas * dracmas;
+  int francos = random();
+  var conversionFrancos = francos * dracmas;
+  print("El valor dracmas $dracmas dracmas a francos franceses es: $conversionFrancos ")
+}
+
+
+void pesetasADolaresYLiras (){
+  int pesetas = 45;
+  double dolares = pesetas / 122499;
+  double liras = pesetas / 0.09289;
+  print("El valo de $pesetas pesetas a dolares es: $dolares y a liras es: $liras")
+}
